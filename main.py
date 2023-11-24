6a887cb87e7a18f704f10ef700f9491bf3aba8fc
gabriel-sparkes <sparkes.gabo@gmail.com>
2023-11-24 18:28:05 +0100

    Added stats and checking for unsupported files

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import re
 import ssl
+import time
 import pickle as p
 import atexit as a
 import smtplib
+import threading
+from datetime import datetime
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler
@@ -25,10 +28,35 @@ with open("users.dat", "rb") as f:
         users = p.load(f)
     except:
         users = {}
+    print(f"Users: {users}")
+
+try:
+    open('stats.dat', 'x')
+except:
+    pass
+
+with open("stats.dat", "rb") as f:
+    try:
+        user_stats = p.load(f)
+    except:
+        user_stats = {"total_sent": 0, "sent_this_month": 0, "monthly_users": [], "total_users": []}
+    print(f"Stats: {user_stats}")
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 KINDLE_EMAIL = os.getenv("KINDLE_EMAIL")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+SUPPORTED_FILETYPES = ["application/pdf", "application/epub+zip", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/msword", "application/rtf", "text/html", "text/plain", "application/zip", "image/jpeg", "image/gif", "image/bmp", "image/png"]
+
+def first_of_month():
+    print("Started checking date")
+    while True:
+        today = datetime.today().day
+        if today == 1:
+            user_stats["monthly_users"] = []
+            user_stats["sent_this_month"] = 0
+        with open("stats.dat", "wb") as f:
+            p.dump(user_stats, f)
+        time.sleep(3600)
 
 def is_valid_email(text: str):
     pattern = r'^[a-zA-Z0-9._%+-]+@kindle\.com$'
@@ -37,7 +65,7 @@ def is_valid_email(text: str):
     else:
         return False
     
-def send_mail(send_from: str, send_to: str, text:str, subject: str, file: str, file_name: str):
+def send_mail(send_from: str, send_to: str, text: str, subject: str, file: str, file_name: str):
     s = smtplib.SMTP_SSL('smtp.gmail.com', 465, context=ssl.create_default_context())
 
     message = MIMEMultipart()
@@ -59,7 +87,7 @@ def send_mail(send_from: str, send_to: str, text:str, subject: str, file: str, f
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     users[update.effective_user.id] = {}
-    await update.message.reply_text(f"Hello {update.effective_user.first_name}!\nI can help you send your documents to your kindle!\nSend /setup for a tutorial on how to enable this bot to send it files and to set your kindle's email\nNB: I will forward any document you send me, but sending PDF and EPUB files only is <b>strongly recommended</b>. Additionally, you'll have to navigate to your <a href=\"https://amazon.com/myk\">Manage Your Content and Devices page</a> to delete documents you've sent to your kindle", parse_mode="HTML")
+    await update.message.reply_text(f"Hello {update.effective_user.first_name}!\nI can help you send your documents to your kindle!\nSend /setup for a tutorial on how to enable this bot to send it files and to set your kindle's email\nNB: I will forward any document you send me, but sending PDF and EPUB files only is <b>strongly recommended</b>. Please note that support for MOBI files will be discontinued soon.\nAdditionally, you'll have to navigate to your <a href=\"https://amazon.com/myk\">Manage Your Content and Devices page</a> to delete documents you've sent to your kindle", parse_mode="HTML")
 
 async def setup(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     users[update.effective_user.id]["setting"] = True
@@ -70,37 +98,35 @@ async def set_email(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("OK, send me your email in the next message")
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if users[update.effective_user.id]["setting"]:
-        users[update.effective_user.id]["setting"] = False
+    user_id = update.effective_user.id
+    if users[user_id]["setting"]:
+        users[user_id]["setting"] = False
         await update.message.reply_text("Cancelled")
     else:
         await update.message.reply_text("Nothing to cancel!")
 
 async def forget(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if "email" in users[update.effective_user.id]:
-        del users[update.effective_user.id]["email"]
+    user_id = update.effective_user.id
+    if "email" in users[user_id]:
+        del users[user_id]["email"]
         await update.message.reply_text("Your email address has been removed")
     else:
         await update.message.reply_text("Nothing to forget!")
-        
-async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not "setting" in users[update.effective_user.id]:
-        users[update.effective_user.id]["setting"] = False
-    if users[update.effective_user.id]["setting"]:
-        if is_valid_email(update.message.text):
-            users[update.effective_user.id]["email"] = update.message.text
-            await update.message.reply_text(f"Successfully set your email to <i>{update.message.text}</i>!", parse_mode="HTML")
-            users[update.effective_user.id]["setting"] = False
-        else:
-            await update.message.reply_text("Please enter a valid kindle email address (must end in '@kindle.com'!)")
 
 async def document(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not "email" in users[update.effective_user.id]:
+    user_id = update.effective_user.id
+    if user_id not in users:
+        users[user_id] = {}
+    if not "email" in users[user_id]:
         await update.message.reply_text("Email is not set!")
     else:
         await update.message.reply_text("Working on it...")
         try:
-            to = users[update.effective_user.id]["email"]
+            mime_type = update.message.document.mime_type
+            if mime_type not in SUPPORTED_FILETYPES:
+                await update.message.reply_text("It looks like your file is not supported by kindle. Remember that PDF and EPUB files have the best support")
+                return
+            to = users[user_id]["email"]
             file_id = update.message.document.file_id
             file = await context.bot.get_file(file_id)
             file_name = update.message.document.file_name
@@ -108,13 +134,42 @@ async def document(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             send_mail(KINDLE_EMAIL, to, "Here's your file! :)", "Your file, sent via @sendtokindle_robot on telegram", path, file_name)
             os.remove(path)
             await update.message.reply_text("Your file has been sent successfully!")
+            user_stats["total_sent"] += 1
+            user_stats["sent_this_month"] += 1
+            if user_id not in user_stats["monthly_users"]:
+                user_stats["monthly_users"].append(user_id)
+            if user_id not in user_stats["total_users"]:
+                user_stats["total_users"].append(user_id)
         except Exception as e:
             await update.message.reply_text("An error occurred 😞")
             print(e)
 
+async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE, user_stats=user_stats) -> None:
+    if user_stats == {}:
+        user_stats = {"total_sent": 0, "sent_this_month": 0, "monthly_users": [], "total_users": []}
+    await update.message.reply_text(f"<b><i>Stats</i></b>\n<b>Total number of documents sent: </b><i>{user_stats['total_sent']}</i>\n<b>Number of documents sent this month: </b><i>{user_stats['sent_this_month']}</i>\n<b>Total number of users: </b><i>{len(user_stats['total_users'])}</i>\n<b>Number of users this month: </b><i>{len(user_stats['monthly_users'])}</i>", parse_mode="HTML")
+
+async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.effective_user.id
+    if user_id not in users:
+        users[user_id] = {}
+    if not "setting" in users[user_id]:
+        users[user_id]["setting"] = False
+    if not "id" in users[user_id]:
+        users[user_id]["id"] = user_id
+    if users[user_id]["setting"]:
+        if is_valid_email(update.message.text):
+            users[user_id]["email"] = update.message.text
+            await update.message.reply_text(f"Successfully set your email to <i>{update.message.text}</i>!", parse_mode="HTML")
+            users[user_id]["setting"] = False
+        else:
+            await update.message.reply_text("Please enter a valid kindle email address (must end in '@kindle.com'!)")
+
 def exit_handler():
     with open("users.dat", "wb") as f:
         p.dump(users, f)
+    with open("stats.dat", "wb") as f:
+        p.dump(user_stats, f)
     print("Goodbye!")
 
 app = ApplicationBuilder().token(TOKEN).build()
@@ -124,8 +179,13 @@ app.add_handler(CommandHandler("setup", setup))
 app.add_handler(CommandHandler("set", set_email))
 app.add_handler(CommandHandler("cancel", cancel))
 app.add_handler(CommandHandler("forget", forget))
+app.add_handler(CommandHandler("stats", stats))
 app.add_handler(MessageHandler(TEXT, message))
 app.add_handler(MessageHandler(Document.ALL, document))
 a.register(exit_handler)
+
+thread = threading.Thread(target=first_of_month)
+thread.daemon = True
+thread.start()
 
 app.run_polling()
